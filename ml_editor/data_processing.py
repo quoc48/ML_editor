6b860545bc7548615149cbd3afdf7230095ef0f3
thanh.quoc731@gmail.com <thanh.quoc731@gmail.com>
2024-12-08 21:31:09 +0700

    adding for raw dataframe to data_processing file

--- ml_editor/data_processing.py
+++ ml_editor/data_processing.py
@@ -1,2 +1,33 @@
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split, GroupShuffleSplit
+from scipy.sparse import vstack, hstack
+
+def format_raw_df(df):
+    """
+    Cleanup data and join questions to answers
+    :param df: raw DataFrame
+    :return: processed DataFrame
+    """
+    # Fixing types and setting index
+    df["PostTypeId"] = df["PostTypeId"].astype(int)
+    df["Id"] = df["Id"].astype(int)
+    df["AnswerCount"] = df["AnswerCount"].fillna(-1)
+    df["AnswerCount"] = df["AnswerCount"].astype(int)
+    df["OwnerUserId"].fillna(-1, inplace=True)
+    df["OwnerUserId"] = df["OwnerUserId"].astype(int)
+    df.set_index("Id", inplace=True, drop=False)
+
+    df["is_question"] = df["PostTypeId"] == 1
+
+    # Filtering out PostTypeIds other than documented ones
+    df = df[df["PostTypeId"].isin([1, 2])]
+
+    # Linking questions and answers
+    df = df.join(
+        df[["Id", "Title", "body_text", "Score", "AcceptedAnswerId"]],
+        on="ParentId",
+        how="left",
+        rsuffix="_question",
+    )
+    return df
