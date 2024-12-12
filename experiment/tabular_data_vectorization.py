b35f60a4c42141e92047daf8df0b81f1d95f5c80
thanh.quoc731@gmail.com <thanh.quoc731@gmail.com>
2024-12-12 23:08:51 +0700

    normalized data input

--- experiment/tabular_data_vectorization.py
+++ experiment/tabular_data_vectorization.py
@@ -9,11 +9,16 @@ warnings.filterwarnings('ignore')
 project_root = Path(__file__).resolve().parent.parent
 sys.path.append(str(project_root))
 
-from ml_editor.data_processing import get_vertorized_series
+from ml_editor.data_processing import get_vertorized_series, get_normalized_series
 
 df = pd.read_csv(Path('data/writers.csv'))
 
 df["is_question"] = df["PostTypeId"] == 1
 tabular_df = df[df["is_question"]][["Tags", "CommentCount", "CreationDate", "Score"]]
+
+tabular_df["NormComment"] = get_normalized_series(tabular_df, "CommentCount")
+tabular_df["NormScore"] = get_normalized_series(tabular_df, "Score")
+
 print(tabular_df.head())
+
  
